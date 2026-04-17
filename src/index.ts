2acb7c97fd5576648ceead6a039dc5d9ec9f8129
Apenas1Hugo <hugorodriguesm27@gmail.com>
2026-04-17 11:20:59 -0300

    GET and POST
    Express instalado, métodos GET e POST criados e testados.

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,47 @@
-import http from "http";
+import express from "express";
 
-const server = http.createServer((req, res) => {
-    if (req.url === "/") {
-        res.writeHead(200, { "Content-Type": "text/plain" });
-        res.end("Tudo certo campeão!");
-    } else if(req.url === "/api"){
-            res.writeHead(200, { "Content-Type": "application/json" });
-            res.end(JSON.stringify({ message: "Bem-vindo à API!" }));
-    } else {
-            res.writeHead(404);
-            res.end("Página não encontrada");
-        }
-    });
+const app = express();
+app.use(express.json());
 
-server.listen(3000, () => {
-    console.log("Servidor rodando na porta 3000");
+let users = [
+  { id: 1, name: "Hugo", email: "hugo@example.com" },
+  { id: 2, name: "Marianna", email: "marianna@example.com" },
+];
 
+// Rota para obter todos os usuários
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+
+// Rota para obter um usuário por ID
+app.get("/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  if (isNaN(id)) {
+    return res.status(400).json({ message: "ID inválido" });
+  }
+  
+  const user = users.find((u) => u.id === id);
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).json({ message: "Usuário não encontrado" });
+  }
+});
+
+// Rota para criar um novo usuário
+app.post("/users", (req, res) => {
+    const { name, email } = req.body;
+    const newUser = {
+        id: users.length + 1,
+        name,
+        email
+    };
+    users.push(newUser);
+    res.status(201).json(newUser);
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
 });
